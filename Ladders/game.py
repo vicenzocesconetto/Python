1ae9975b6ab1c05cd3305b5a88c487c5210bb8c2
vicenzo-cesconetto <vicenzoccesconetto@gmail.com>
2018-07-25 18:53:14 -0300

    meta simulations to get the mean

--- Ladders/game.py
+++ Ladders/game.py
@@ -23,29 +23,41 @@ DICE_SIDES = 6
 
 AMOUNT_OF_SIMULATIONS = 10000
 
-players_victories = [0] * NUMBER_OF_PLAYERS
+NUMBER_OF_META = 2
 
+# Every even position is where the ladder starts and the following position is where is ends
 ladders = [3, 16, 5, 7, 15, 25, 18, 20, 21, 32]
 
-snakes = [12, 2, 14, 11, 17, 4, 31, 19, 35, 22]
+snakes = [12, 2, 14, 11, 17, 4, 31, 19, 35, 22]  # Same idea for the snakes list
+
+am1 = [0] * NUMBER_OF_META
+am2 = am1[:]
+
+for iteration in range(NUMBER_OF_META):
+    players_victories = [0] * NUMBER_OF_PLAYERS
+    for i in range(AMOUNT_OF_SIMULATIONS):
+        players_positions = [INITIAL_SQUARE] * NUMBER_OF_PLAYERS
+        # print('starting players_positions:' + str(players_positions))
+        victory = False
+        while not victory:
+            for index in range(NUMBER_OF_PLAYERS):
+                # print('player ' + str(index) + ' goes from ' + str(players_positions[index]), end='')
+                players_positions[index] += random.randint(1, DICE_SIDES)
+                # print(' to ' + str(players_positions[index]), end='')
+                players_positions[index] = check_for_snakes_and_ladders(snakes, ladders, players_positions[index])
+                # print(' and ends up in ' + str(players_positions[index]))
+                if players_positions[index] >= BOARD_SIZE:
+                    victory = True
+                    players_victories[index] += 1
+                    break
+
+        # print('players positions end of game:' + str(players_positions))
+    am1[iteration] = players_victories[0]
+    am2[iteration] = players_victories[1]
+    print(players_victories)
 
 
-for i in range(AMOUNT_OF_SIMULATIONS):
-    players_positions = [INITIAL_SQUARE] * NUMBER_OF_PLAYERS
-    # print('starting players_positions:' + str(players_positions))
-    victory = False
-    while not victory:
-        for index in range(NUMBER_OF_PLAYERS):
-            # print('player ' + str(index) + ' goes from ' + str(players_positions[index]), end='')
-            players_positions[index] += random.randint(1, DICE_SIDES)
-            # print(' to ' + str(players_positions[index]), end='')
-            players_positions[index] = check_for_snakes_and_ladders(snakes, ladders, players_positions[index])
-            # print(' and ends up in ' + str(players_positions[index]))
-            if players_positions[index] >= BOARD_SIZE:
-                victory = True
-                players_victories[index] += 1
-                break
-
-    print('players positions end of game:' + str(players_positions))
-
-print(players_victories)
+# print(sum(am1))
+print(sum(am1)/len(am1))
+# print(sum(am2))
+print(sum(am2)/len(am2))
