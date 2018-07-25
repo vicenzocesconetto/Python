86e31c5af257e1d80a7aa9050a4570d3c6596905
vicenzo-cesconetto <vicenzoccesconetto@gmail.com>
2018-07-25 20:01:11 -0300

    change users initial position to make the game 'fair'

--- Ladders/game.py
+++ Ladders/game.py
@@ -15,7 +15,12 @@ def check_for_snakes_and_ladders(snakes: list, ladders: list, current_position: 
 
 NUMBER_OF_PLAYERS = 2
 
-INITIAL_SQUARE = 1
+INITIAL_SQUARE = 1  # Used in the initial versions
+
+PLAYERS_INITIAL_POSITION = [1, 1]
+
+# print(type(PLAYERS_INITIAL_POSITION))
+print(PLAYERS_INITIAL_POSITION)
 
 BOARD_SIZE = 36
 
@@ -23,7 +28,7 @@ DICE_SIDES = 6
 
 AMOUNT_OF_SIMULATIONS = 10000
 
-NUMBER_OF_META = 2
+NUMBER_OF_META = 10
 
 # Every even position is where the ladder starts and the following position is where is ends
 ladders = [3, 16, 5, 7, 15, 25, 18, 20, 21, 32]
@@ -36,7 +41,8 @@ am2 = am1[:]
 for iteration in range(NUMBER_OF_META):
     players_victories = [0] * NUMBER_OF_PLAYERS
     for i in range(AMOUNT_OF_SIMULATIONS):
-        players_positions = [INITIAL_SQUARE] * NUMBER_OF_PLAYERS
+        players_positions = PLAYERS_INITIAL_POSITION[:]
+        # print(players_positions)
         # print('starting players_positions:' + str(players_positions))
         victory = False
         while not victory:
@@ -54,10 +60,8 @@ for iteration in range(NUMBER_OF_META):
         # print('players positions end of game:' + str(players_positions))
     am1[iteration] = players_victories[0]
     am2[iteration] = players_victories[1]
-    print(players_victories)
+    # print(players_victories)
 
 
-# print(sum(am1))
-print(sum(am1)/len(am1))
-# print(sum(am2))
-print(sum(am2)/len(am2))
+print('player 1 arithmetic mean = ' + str(sum(am1)/len(am1)))
+print('player 2 arithmetic mean = ' + str(sum(am2)/len(am2)))
