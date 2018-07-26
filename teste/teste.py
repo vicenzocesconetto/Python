9484a90ef08c60d11c0e20e07197fe87a1a192e8
vicenzo-cesconetto <vicenzoccesconetto@gmail.com>
2018-07-26 18:25:32 -0300

    working on the model and ancillary functionalities

--- teste/teste.py
+++ teste/teste.py
@@ -2,20 +2,19 @@
 def setup(LEARNED_VALUES_FILENAME: str, NUMBER_OF_SEEN_CASES_FILENAME: str, medical_conditions_weights: list) -> int:
     """Loads the values learned and the number of seen cases"""
     try:
-        with open(LEARNED_VALUES_FILENAME, 'r') as LEARNED_VALUES_FILE:
-            for line in LEARNED_VALUES_FILE:
+        with open(LEARNED_VALUES_FILENAME, 'r') as learned_values:
+            for line in learned_values:
                 medical_conditions_weights.append(int(line))
 
     except IOError:
         open(LEARNED_VALUES_FILENAME, "w").close()
         print(LEARNED_VALUES_FILENAME + ' was created')
 
-
     number_of_seen_cases = 0
 
     try:
-        with open(NUMBER_OF_SEEN_CASES_FILENAME, 'r') as NUMBER_OF_SEEN_CASES_FILE:
-            for line in NUMBER_OF_SEEN_CASES_FILE:
+        with open(NUMBER_OF_SEEN_CASES_FILENAME, 'r') as seen_cases_file:
+            for line in seen_cases_file:
                 number_of_seen_cases = int(line)
 
     except IOError:
@@ -30,34 +29,54 @@ def parse_line(line: str) -> list:
     return line.split(',')
 
 
-def feed(DATASET_FILENAME: str):
+def feed(dataset_filename: str, number_of_seen_cases: int):
     """This function is supposed to feed the data to the model"""
-    with open(DATASET_FILENAME, 'r') as dataset:
+    with open(dataset_filename, 'r') as dataset:
         for line in dataset:
             dataset_line_values = parse_line(line)
 
 
-def __main__():
-    print('Power to the main thrusters')
+def learn(case_values: list, number_of_seen_cases: int):
+    if case_values[-1] == 0:
+        return
 
-    LEARNED_VALUES_FILENAME = 'learned.txt'
 
-    NUMBER_OF_SEEN_CASES_FILENAME = 'seen_cases.txt'
 
-    medical_conditions_weights = list()
+def shutdown(learned_values_filename: str, learned_values: list, number_of_seen_cases_filename: str, seen_cases: int):
 
-    print('Initializing engines')
 
-    number_of_seen_cases = setup(LEARNED_VALUES_FILENAME, NUMBER_OF_SEEN_CASES_FILENAME, medical_conditions_weights)
+print('Power to the main thrusters')
 
-    if len(medical_conditions_weights) == 0:
-        size = input('seems like the files dint exist, did they? Well, what is the amount of conditions?\n')
-        for i in range(0, int(size)):
-            medical_conditions_weights.append(0)
 
-    command = 1
+LEARNED_VALUES_FILENAME = 'learned.txt'
 
-    while command:
-        command = int(input('what do you want to do?\n'
-                        '[0]Exit\n'
-                        '[1]Train the model\n'))
+NUMBER_OF_SEEN_CASES_FILENAME = 'seen_cases.txt'
+
+CSV_LINE_LENGTH = 7
+
+AMOUNT_OF_GOAL_VALUES = 1
+
+dataset_filename = 'my-heart.csv'
+
+medical_conditions_weights = list()
+
+print('Initializing engines')
+
+number_of_seen_cases = setup(LEARNED_VALUES_FILENAME, NUMBER_OF_SEEN_CASES_FILENAME, medical_conditions_weights)
+
+if len(medical_conditions_weights) == 0:
+    print('Seems like the files didnt exist')
+    medical_conditions_weights = [0] * (CSV_LINE_LENGTH - AMOUNT_OF_GOAL_VALUES)
+
+command = 1
+
+while command:
+    command = int(input('what do you want to do?\n'
+                    '[0]Exit\n'
+                    '[1]Train the model\n'))
+
+    if command == 0:
+
+
+    if command == 1:
+        feed(dataset_filename, number_of_seen_cases)
